09dec62370b012bcd1adbf32bfc77fa51c385dea
DicksonEplayer <yourexactplayer@gmail.com>
2026-01-28 22:08:37 +0300

    Update Footer.tsx to remove stack and instead put dev name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     <footer className="bg-white mt-auto">
       <div className="container mx-auto py-6 px-4 sm:px-6 lg:px-8 text-center text-gray-500">
         <p>&copy; {new Date().getFullYear()} Ainamani Dickson. All rights reserved.</p>
-        <p className="text-sm mt-1">Built with React & Tailwind CSS.</p>
+        <p className="text-sm mt-1">Built by Ainamani.</p>
       </div>
     </footer>
   );
